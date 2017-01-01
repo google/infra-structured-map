e7bfd2a9463725c02c51bdc8a4989d7fb5eab349
Brian Ferris <bdferris@bdferris-macbookair2.roam.corp.google.com>
2016-12-31 22:27:59 -0800

    Re-order class definitions.

--- src/main/js/infra-structured-map.js
+++ src/main/js/infra-structured-map.js
@@ -15,251 +15,6 @@
  */
 (function(root) {
 
-  function channelMasksAreEqual(lhs, rhs) {
-    if (lhs.length != rhs.length) {
-      return false;
-    }
-    for (var i = 0; i < lhs.length; ++i) {
-      if (lhs[i] != rhs[i]) {
-	return false;
-      }
-    }
-    return true;
-  }
-
-
-  function PropertyMask() {
-    this.mask = 0;
-    return this;
-  };
-
-  PropertyMask.prototype.isActive = function(rhs) {
-    return (this.mask & rhs.mask) != 0;
-  };
-
-  PropertyMask.prototype.setEnabled = function(index, enabled) {
-    if (enabled) {
-      this.mask |= 1 << index;
-    } else {
-      this.mask &= ~(1 << index);
-    }
-  };
-
-  PropertyMask.prototype.setEnabledAll = function(enabled) {
-    if (enabled) {
-      this.mask = ~0;
-    } else {
-      this.mask = 0;
-    }
-  };
-
-  PropertyMask.prototype.isEnabled = function(index) {
-    return (this.mask >> index) & 1;
-  };
-
-  PropertyMask.prototype.toString = function() {
-    return (this.mask >>> 0).toString(2);
-  }
-
-  function PropertyMasks() {
-    this.mode = new PropertyMask();
-    this.status = new PropertyMask();
-    this.timeline = new PropertyMask();
-    return this;
-  };
-
-  PropertyMasks.prototype.isActive = function(rhs) {
-    return this.mode.isActive(rhs.mode)
-      && this.status.isActive(rhs.status)
-      && this.timeline.isActive(rhs.timeline);
-  };
-
-  PropertyMasks.prototype.toString = function() {
-    return 'mode: ' + this.mode.toString()
-      + ' status: ' + this.status.toString()
-      + ' time: ' + this.timeline.toString();
-  }
-
-  function PropertyIds(propertyIds) {
-    this.idToIndex = new Map();
-    for (var propertyId of propertyIds) {
-      this.idToIndex.set(propertyId, this.idToIndex.size);
-    }
-    return this;
-  }
-
-  PropertyIds.prototype.getIndex = function(id) {
-    return this.idToIndex.get(id);
-  };
-
-  function ProjectRef(propertyMasks, titles, color) {
-    this.propertyMasks = propertyMasks;
-    this.titles = titles;
-    this.color = color;
-    return this;
-  };
-
-  function ProjectRefs() {
-    this.refs = [];
-  }
-
-  ProjectRefs.prototype.push = function(projectRef) {
-    this.refs.push(projectRef);
-  };
-
-  ProjectRefs.prototype.isActive = function(propertyMasks) {
-    for (var projectRef of this.refs) {
-      if (projectRef.propertyMasks.isActive(propertyMasks)) {
-	return true;
-      }
-    }
-    return false;
-  };
-
-
-  function MapChannel(color) {
-    this.projectRefs = new ProjectRefs();
-    this.color = color;
-    return this;
-  };
-
-
-  function MapFeature(map, channels) {
-    this.map = map;
-    this.channels = channels;
-    this.channelMask = [];
-    this.activeChannelCount = 0;
-    this.masks = new PropertyMasks();
-    return this;
-  }
-
-  MapFeature.prototype.updateChannelMask = function(propertyMasks) {
-    this.masks = propertyMasks;
-    var newMask = [];
-    var activeChannelCount = 0;
-    for (var channel of this.channels) {
-      var active = channel.projectRefs.isActive(propertyMasks);
-      newMask.push(active);
-      if (active) {
-	activeChannelCount++;
-      }
-    }
-    if (channelMasksAreEqual(this.channelMask, newMask)) {
-      return false;
-    }
-    this.channelMask = newMask;
-    this.activeChannelCount = activeChannelCount;
-    return true;
-  }
-
-  MapFeature.prototype.handleClick_ = function(event) {
-    var content = '';
-    for (var index = 0; index < this.channels.length; ++index) {
-      if (!this.channelMask[index]) {
-	continue;
-      }
-      var channel = this.channels[index];
-      for (var projectRef of channel.projectRefs.refs) {
-	if (projectRef.propertyMasks.isActive(this.masks)) {
-	  content += projectRef.titles.join(' - ') + ' <br/>\n';
-	}
-      }
-    }
-    this.map.showInfoWindow(content, event.latLng);
-  }
-
-  function MapSegment(map, channels, segment) {
-    MapFeature.call(this, map, channels);
-
-    var path = google.maps.geometry.encoding.decodePath(segment.line);
-    this.options = {
-      path: path,
-      strokeOpacity: 0,
-      icons: [],
-      map: map,
-      clickable: true
-    };
-    this.polyline = new google.maps.Polyline(this.options);
-    google.maps.event.addListener(
-      this.polyline, 'click', this.handleClick_.bind(this));
-  };
-
-  MapSegment.prototype = new MapFeature();
-
-  MapSegment.prototype.updateChannels = function(propertyMasks) {
-    if (!this.updateChannelMask(propertyMasks)) {
-      return;
-    }
-    var offset = -(this.activeChannelCount - 1) / 2;
-    var icons = new Array();
-    for (var index = 0; index < this.channels.length; ++index) {
-      if (!this.channelMask[index]) {
-	continue;
-      }
-      var channel = this.channels[index];
-      var symbol = {
-	path: 'M ' + offset + ',-0.2 ' + offset + ',0',
-	strokeOpacity: 1,
-	strokeColor: channel.color,
-	scale: 5
-      };
-      icons.push({icon: symbol, offset: '0', repeat: '2px'});
-      offset++;
-    }
-    this.options.icons = icons;
-    this.polyline.setOptions(this.options);
-  };
-
-  function MapPlacemark(map, channels, position) {
-    MapFeature.call(this, map, channels);
-    this.position = position;
-    this.markers = [];
-    return this;
-  };
-
-  MapPlacemark.prototype = new MapFeature();
-
-  MapPlacemark.prototype.updateChannels = function(propertyMasks) {
-    if (!this.updateChannelMask(propertyMasks)) {
-      return;
-    }
-
-    // Clear any existing markers
-    for (var marker of this.markers) {
-      marker.setMap(null);
-    }
-    this.markers = [];
-
-    var offset = 0;
-    for (var index = 0; index < this.channels.length; ++index) {
-      if (!this.channelMask[index]) {
-	continue;
-      }
-      var channel = this.channels[index];
-
-      var circle = {
-	path: 'M ' + offset + ',0 m -10,0 a 10,10 0 1,0 20,0 a 10,10 0 1,0 -20,0',
-	fillColor: channel.color,
-	fillOpacity: 1.0,
-	scale: 1,
-	strokeColor: 'white',
-	strokeWeight: 2
-      };
-      var options = {
-	position: this.position,
-	icon: circle,
-	map: this.map,
-	clickable: true
-      };
-      var marker = new google.maps.Marker(options);
-      this.markers.push(marker);
-      offset += 10;
-
-      google.maps.event.addListener(
-	marker, 'click', this.handleClick_.bind(this));
-    }
-  };
-
   function InfraStructuredMap(map) {
     this.map = map;
 
@@ -381,5 +136,248 @@
   };
 
   root.InfraStructuredMap = InfraStructuredMap;
+
+
+  function MapFeature(map, channels) {
+    this.map = map;
+    this.channels = channels;
+    this.channelMask = [];
+    this.activeChannelCount = 0;
+    this.masks = new PropertyMasks();
+    return this;
+  }
+
+  MapFeature.prototype.updateChannelMask = function(propertyMasks) {
+    this.masks = propertyMasks;
+    var newMask = [];
+    var activeChannelCount = 0;
+    for (var channel of this.channels) {
+      var active = channel.projectRefs.isActive(propertyMasks);
+      newMask.push(active);
+      if (active) {
+	activeChannelCount++;
+      }
+    }
+    if (channelMasksAreEqual(this.channelMask, newMask)) {
+      return false;
+    }
+    this.channelMask = newMask;
+    this.activeChannelCount = activeChannelCount;
+    return true;
+  }
+
+  MapFeature.prototype.handleClick_ = function(event) {
+    var content = '';
+    for (var index = 0; index < this.channels.length; ++index) {
+      if (!this.channelMask[index]) {
+	continue;
+      }
+      var channel = this.channels[index];
+      for (var projectRef of channel.projectRefs.refs) {
+	if (projectRef.propertyMasks.isActive(this.masks)) {
+	  content += projectRef.titles.join(' - ') + ' <br/>\n';
+	}
+      }
+    }
+    this.map.showInfoWindow(content, event.latLng);
+  }
+
+  function channelMasksAreEqual(lhs, rhs) {
+    if (lhs.length != rhs.length) {
+      return false;
+    }
+    for (var i = 0; i < lhs.length; ++i) {
+      if (lhs[i] != rhs[i]) {
+	return false;
+      }
+    }
+    return true;
+  }
+
+  function MapSegment(map, channels, segment) {
+    MapFeature.call(this, map, channels);
+
+    var path = google.maps.geometry.encoding.decodePath(segment.line);
+    this.options = {
+      path: path,
+      strokeOpacity: 0,
+      icons: [],
+      map: map,
+      clickable: true
+    };
+    this.polyline = new google.maps.Polyline(this.options);
+    google.maps.event.addListener(
+      this.polyline, 'click', this.handleClick_.bind(this));
+  };
+
+  MapSegment.prototype = new MapFeature();
+
+  MapSegment.prototype.updateChannels = function(propertyMasks) {
+    if (!this.updateChannelMask(propertyMasks)) {
+      return;
+    }
+    var offset = -(this.activeChannelCount - 1) / 2;
+    var icons = new Array();
+    for (var index = 0; index < this.channels.length; ++index) {
+      if (!this.channelMask[index]) {
+	continue;
+      }
+      var channel = this.channels[index];
+      var symbol = {
+	path: 'M ' + offset + ',-0.2 ' + offset + ',0',
+	strokeOpacity: 1,
+	strokeColor: channel.color,
+	scale: 5
+      };
+      icons.push({icon: symbol, offset: '0', repeat: '2px'});
+      offset++;
+    }
+    this.options.icons = icons;
+    this.polyline.setOptions(this.options);
+  };
+
+  function MapPlacemark(map, channels, position) {
+    MapFeature.call(this, map, channels);
+    this.position = position;
+    this.markers = [];
+    return this;
+  };
+
+  MapPlacemark.prototype = new MapFeature();
+
+  MapPlacemark.prototype.updateChannels = function(propertyMasks) {
+    if (!this.updateChannelMask(propertyMasks)) {
+      return;
+    }
+
+    // Clear any existing markers
+    for (var marker of this.markers) {
+      marker.setMap(null);
+    }
+    this.markers = [];
+
+    var offset = 0;
+    for (var index = 0; index < this.channels.length; ++index) {
+      if (!this.channelMask[index]) {
+	continue;
+      }
+      var channel = this.channels[index];
+
+      var circle = {
+	path: 'M ' + offset + ',0 m -10,0 a 10,10 0 1,0 20,0 a 10,10 0 1,0 -20,0',
+	fillColor: channel.color,
+	fillOpacity: 1.0,
+	scale: 1,
+	strokeColor: 'white',
+	strokeWeight: 2
+      };
+      var options = {
+	position: this.position,
+	icon: circle,
+	map: this.map,
+	clickable: true
+      };
+      var marker = new google.maps.Marker(options);
+      this.markers.push(marker);
+      offset += 10;
+
+      google.maps.event.addListener(
+	marker, 'click', this.handleClick_.bind(this));
+    }
+  };
+
+  function MapChannel(color) {
+    this.projectRefs = new ProjectRefs();
+    this.color = color;
+    return this;
+  };
+
+  function ProjectRef(propertyMasks, titles, color) {
+    this.propertyMasks = propertyMasks;
+    this.titles = titles;
+    this.color = color;
+    return this;
+  };
+
+  function ProjectRefs() {
+    this.refs = [];
+  }
+
+  ProjectRefs.prototype.push = function(projectRef) {
+    this.refs.push(projectRef);
+  };
+
+  ProjectRefs.prototype.isActive = function(propertyMasks) {
+    for (var projectRef of this.refs) {
+      if (projectRef.propertyMasks.isActive(propertyMasks)) {
+	return true;
+      }
+    }
+    return false;
+  };
+
+  function PropertyIds(propertyIds) {
+    this.idToIndex = new Map();
+    for (var propertyId of propertyIds) {
+      this.idToIndex.set(propertyId, this.idToIndex.size);
+    }
+    return this;
+  }
+
+  PropertyIds.prototype.getIndex = function(id) {
+    return this.idToIndex.get(id);
+  };
+
+  function PropertyMask() {
+    this.mask = 0;
+    return this;
+  };
+
+  PropertyMask.prototype.isActive = function(rhs) {
+    return (this.mask & rhs.mask) != 0;
+  };
+
+  PropertyMask.prototype.setEnabled = function(index, enabled) {
+    if (enabled) {
+      this.mask |= 1 << index;
+    } else {
+      this.mask &= ~(1 << index);
+    }
+  };
+
+  PropertyMask.prototype.setEnabledAll = function(enabled) {
+    if (enabled) {
+      this.mask = ~0;
+    } else {
+      this.mask = 0;
+    }
+  };
+
+  PropertyMask.prototype.isEnabled = function(index) {
+    return (this.mask >> index) & 1;
+  };
+
+  PropertyMask.prototype.toString = function() {
+    return (this.mask >>> 0).toString(2);
+  }
+
+  function PropertyMasks() {
+    this.mode = new PropertyMask();
+    this.status = new PropertyMask();
+    this.timeline = new PropertyMask();
+    return this;
+  };
+
+  PropertyMasks.prototype.isActive = function(rhs) {
+    return this.mode.isActive(rhs.mode)
+      && this.status.isActive(rhs.status)
+      && this.timeline.isActive(rhs.timeline);
+  };
+
+  PropertyMasks.prototype.toString = function() {
+    return 'mode: ' + this.mode.toString()
+      + ' status: ' + this.status.toString()
+      + ' time: ' + this.timeline.toString();
+  }
 
 }(this));
